61afd28c91bef1052e59df1c6f26e19d0356e555
Maha Malik <malikms@uci.edu>
2020-05-26 00:09:17 -0700

    Initial Commit

---
+++ ForageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForageApp
+//
+//  Created by Maha Malik on 5/26/20.
+//  Copyright © 2020 Forage-UCI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
