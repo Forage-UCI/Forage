0bf157d0904c2e43d5fb3ef1b88aa70d34d93bea
Maha Malik <malikms@uci.edu>
2020-05-26 22:42:00 -0700

    Google Place API attempt 1

--- ForageApp/AppDelegate.swift
+++ ForageApp/AppDelegate.swift
@@ -22,6 +22,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://damp-brushlands-48529.herokuapp.com/parse"
             })
         )
+        
+        /*https://maps.googleapis.com/maps/api/place/nearbysearch/json
+        ?location=-33.8670522,151.1957362
+        &radius=500
+        &types=food
+        &name=harbour
+        &key=YOUR_API_KEY*/
+        
         return true
     }
 
