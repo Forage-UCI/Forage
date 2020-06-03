3a0d53b7f4c4be05047a38bf9927f42874fc1b3f
Yanjie Xu <yanjiexu06@gmail.com>
2020-06-03 15:24:00 +0800

    Re-organised project

---
+++ ForageApp/CheckInView/CheckInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CheckInViewController.swift
+//  Forage
+//
+//  Created by Yanjie Xu on 2020/6/3.
+//  Copyright © 2020 Forage-UCI. All rights reserved.
+//
+
+import UIKit
+
+class CheckInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
